d8cd7fadb3b376018330db8bf65f4df4414bf4c7
asura7969 <1402357969@qq.com>
2022-09-20 23:26:46 +0800

    [test]-deltalake

--- src/delta/mod.rs
+++ src/delta/mod.rs
@@ -5,10 +5,31 @@
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+    use std::sync::Arc;
+    use deltalake::{DeltaTable, DeltaTableBuilder, DeltaTableConfig};
+    use deltalake::storage::DeltaObjectStore;
+    use deltalake::writer::{DeltaWriter, RecordBatchWriter};
+    use deltalake::writer::test_utils::get_record_batch;
 
     #[test]
     fn open_table() {
-        // let table = deltalake::open_table("./tests/data/simple_table").await.unwrap();
-        // println!("{}", table.get_files());
+        let table_uri = "file:///foo/bar";
+
+        let config = HashMap::new();
+        let backend: Arc<DeltaObjectStore> = DeltaTableBuilder::from_uri(table_uri)
+            .with_storage_options(config)
+            .build_storage()
+            .unwrap();
+
+        let batch = get_record_batch(None, false);
+
+        let mut table = DeltaTable::new(backend, DeltaTableConfig::default()).unwrap();
+        let mut writer = RecordBatchWriter::for_table(&table).unwrap();
+
+        writer.write(batch).await.unwrap();
+        let adds = writer.flush().await.unwrap();
+
+        assert_eq!(adds.len(), 1);
     }
 }
